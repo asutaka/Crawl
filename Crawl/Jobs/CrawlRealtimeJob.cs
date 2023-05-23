13784a4974b842dc50ccb9b600caf3f755c5ae07
phunv@ftech.ai <phunv@ftech.ai>
2023-05-24 00:12:22 +0700

    some update CrawlRealtimeJob

--- Crawl/Jobs/CrawlRealtimeJob.cs
+++ Crawl/Jobs/CrawlRealtimeJob.cs
@@ -14,17 +14,67 @@ namespace Crawl.Jobs
     {
         public void Execute(IJobExecutionContext context)
         {
-            Console.WriteLine("Here");
+            var options = new ChromeOptions();
+            options.AddArguments("headless");
+            using (var driver = new ChromeDriver(options))
+            {
+                var lstLink = new List<string>();
+                try
+                {
+                    driver.Navigate().GoToUrl("https://www.tratencongty.com/?page=1");
+                    var doc = new HtmlDocument();
+                    doc.LoadHtml(driver.PageSource);
+                    var index = 1;
+                    var emptyData = 0;
+                    do
+                    {
+                        var node = doc.DocumentNode.SelectSingleNode($"/html/body/div/div[1]/div[3]/div[{index}]/a");
+                        if (node != null)
+                        {
+                            string hrefValue = node.Attributes["href"].Value.Trim();
+                            if (!string.IsNullOrWhiteSpace(hrefValue))
+                            {
+                                lstLink.Add(hrefValue);
+                            }
+                            emptyData = 1;
+                        }
+                        else
+                        {
+                            emptyData++;
+                        }
 
-            //var options = new ChromeOptions();
-            //options.AddArguments("headless");
-            //using (var driver = new ChromeDriver(options))
-            //{
-            //    driver.Navigate().GoToUrl("https://www.tratencongty.com/?page=1");
-            //    var doc = new HtmlDocument();
-            //    doc.LoadHtml(driver.PageSource);
-            //    var node = doc.DocumentNode.SelectSingleNode("//head/title");
-            //}
+                        index++;
+                        if (emptyData >= 3)
+                        {
+                            index = -1;
+                        }
+                    }
+                    while (index > 0);
+                }
+                catch(Exception ex)
+                {
+                    NLogLogger.PublishException(ex, $"CrawlRealtimeJob.Execute|EXCEPTION| {ex.Message}");
+                }
+
+                foreach (var item in lstLink)
+                {
+                    driver.Navigate().GoToUrl(item);
+                    var doc = new HtmlDocument();
+                    doc.LoadHtml(driver.PageSource);
+                    var nodeTenCongTy = doc.DocumentNode.SelectSingleNode($"/html/body/div/div[1]/div[3]/h4/a/span");
+                    var nodeLoaiHinhHoatDong = doc.DocumentNode.SelectSingleNode($"/html/body/div/div[1]/div[3]/text()[1]");
+                    var nodeMaSoThue = doc.DocumentNode.SelectSingleNode($"/html/body/div/div[1]/div[3]/a");
+                    var nodeDiaChi = doc.DocumentNode.SelectSingleNode($"/html/body/div/div[1]/div[3]/text()[3]");
+                    var nodeDaiDienPhapLuat = doc.DocumentNode.SelectSingleNode($"/html/body/div/div[1]/div[3]/text()[4]");
+                    var nodeNgayCap = doc.DocumentNode.SelectSingleNode($"/html/body/div/div[1]/div[3]/text()[5]");
+                    var nodeNgayHoatDong = doc.DocumentNode.SelectSingleNode($"/html/body/div/div[1]/div[3]/text()[6]");
+                    var nodeDienThoai = doc.DocumentNode.SelectSingleNode($"/html/body/div/div[1]/div[3]/img");
+                    var nodeTrangThai = doc.DocumentNode.SelectSingleNode($"/html/body/div/div[1]/div[3]/text()[9]");
+                }
+                
+
+                var tmp = 1;
+            }
             ////using (var client = new WebClient())
             ////{
             ////    string htmlCode = client.DownloadString($"https://www.tratencongty.com/?page=1");
